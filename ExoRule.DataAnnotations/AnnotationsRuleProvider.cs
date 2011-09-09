ca8f5ab82a315b7e2c3bff53cb07cf153652761e
Keith <klaforce@gmail.com>
2011-09-09 15:13:45 -0400

    Adding new Rule type ListLength to allow the length of arrays to be validated with a static length or compared against a property.  Modified the Game edit screen in LeisureStar to use this functionality.

--- ExoRule.DataAnnotations/AnnotationsRuleProvider.cs
+++ ExoRule.DataAnnotations/AnnotationsRuleProvider.cs
@@ -136,6 +136,10 @@ namespace ExoRule.DataAnnotations
 						foreach (var attr in property.GetAttributes<RangeAttribute>().Take(1))
 							rules.Add(new RangeRule(type.Name, property.Name, (IComparable)attr.Minimum, (IComparable)attr.Maximum, label, GetFormat<IComparable>(property)));
 
+						// ListLength Attribute
+						foreach (var attr in property.GetAttributes<ListLengthAttribute>().Take(1))
+							rules.Add(new ListLengthRule(type.Name, property.Name, attr.StaticLength, attr.LengthCompareProperty, attr.CompareOp, label, () => attr.LengthCompareProperty));
+
 						// Allowed Values Attribute
 						GraphReferenceProperty reference = property as GraphReferenceProperty;
 						if (reference != null)
---
+++ ExoRules/Validation/ListLengthRule.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Runtime.Serialization;
+using ExoGraph;
+using ExoRule;
+
+namespace ExoRule.Validation
+{
+	/// <summary>
+	/// Applies conditions when the length of a list of a <see cref="GraphProperty"/> is
+	/// too short or long.
+	/// </summary>
+	[DataContract(Name = "listLength")]
+	public class ListLengthRule : PropertyRule
+	{
+		#region Fields
+
+		PathSource compareLengthSource;
+
+		#endregion
+
+		#region Constructors
+
+		public ListLengthRule(string rootType, string property, int staticLength, string compareLengthSource, CompareOperator op, Func<string> label, Func<string> compareLabel)
+			: this(rootType, property, staticLength, compareLengthSource, op, label, compareLabel, RuleInvocationType.PropertyChanged)
+		{ }
+
+		public ListLengthRule(string rootType, string property, int staticLength, string compareLengthSource, CompareOperator op, Func<string> label, Func<string> compareLabel, RuleInvocationType invocationTypes)
+			: base(rootType, property, CreateError(rootType, property, staticLength, compareLengthSource, op, label, compareLabel), invocationTypes)
+		{
+			this.StaticLength = staticLength;
+			this.CompareLengthSource = compareLengthSource;
+			this.CompareOperator = op;
+		}
+
+		#endregion
+
+		#region Properties
+
+		[DataMember(Name = "staticLength", EmitDefaultValue = false)]
+		public int StaticLength { get; private set; }
+
+		/// <summary>
+		/// The static or instance path of the property to compare to.
+		/// </summary>
+		[DataMember(Name = "compareLengthSource")]
+		public string CompareLengthSource
+		{
+			get
+			{
+				return compareLengthSource == null ? "" : compareLengthSource.Path;
+			}
+			private set
+			{
+				if (!String.IsNullOrEmpty(value))
+					compareLengthSource = new PathSource(Property.DeclaringType, value);
+				else
+					compareLengthSource = null;
+			}
+		}
+
+		/// <summary>
+		/// The type of comparison to perform.
+		/// </summary>
+		public CompareOperator CompareOperator
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// Indicates whether the compare source is static.
+		/// </summary>
+		public bool CompareLengthSourceIsStatic
+		{
+			get
+			{
+				//return true when the compare property does not exist because we do not 
+				//want the code using this property tack a "this." on the front of the property path
+				return compareLengthSource == null ? true : compareLengthSource.IsStatic;
+			}
+		}
+		#endregion
+
+		#region Methods
+
+		static Error CreateError(string rootType, string property, int staticLength, string compareLengthSource, CompareOperator op, Func<string> label, Func<string> compareLabel)
+		{
+			string message;
+			switch (op)
+			{
+				case CompareOperator.Equal:
+					message = "compare-equal";
+					break;
+				case CompareOperator.NotEqual:
+					message = "compare-not-equal";
+					break;
+				case CompareOperator.GreaterThan:
+					message = "compare-greater-than";
+					break;
+				case CompareOperator.GreaterThanEqual:
+					message =  "compare-greater-than-or-equal";
+					break;
+				case CompareOperator.LessThan:
+					message =  "compare-less-than";
+					break;
+				case CompareOperator.LessThanEqual:
+					message = "compare-less-than-or-equal";
+					break;
+				default:
+					throw new ArgumentException("Invalid comparison operator for list length rule");
+			}
+
+			return new Error(
+				GetErrorCode(rootType, property, "ListLength"), message, typeof(ListLengthRule),
+				(s) => s
+					.Replace("{property}", label())
+					.Replace("{staticLength}", staticLength.ToString())
+					.Replace("{compareSource}", compareLabel()));
+		}
+
+		protected override bool ConditionApplies(GraphInstance root)
+		{
+			//if the Property is not a list then this rule cannot apply
+			if (!Property.IsList)
+				return false;
+
+			//value should be the list you are comparing against
+			object value = root[Property];
+
+			//get the exograph list representation
+			GraphInstanceList items = root.GetList((GraphReferenceProperty)Property);
+
+			if (value == null)
+				return false;
+
+			int lengthToCompareAgainst = 0;
+
+			//now see if the rule is using a static length or the compare property
+			if (StaticLength > 0)
+			{
+				lengthToCompareAgainst = StaticLength;
+			}
+			else
+			{
+				object compareVal = compareLengthSource.GetValue(root);
+				bool success = Int32.TryParse(compareVal.ToString(), out lengthToCompareAgainst);
+
+				//if it could not parse then this is not a valid integer property
+				if (!success)
+					return false;
+			}
+
+			bool? result = CompareRule.Compare(root, items.Count, CompareOperator, lengthToCompareAgainst);
+			return !result.HasValue ? false : result.Value;
+		}
+
+		protected override string TypeName
+		{
+			get
+			{
+				return "listLength";
+			}
+		}
+
+		#endregion
+	}
+}
